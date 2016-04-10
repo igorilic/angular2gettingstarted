d64d6daddd60a055485a3d39281933b6ac32430c
Igor Ilic <ilicigor@outlook.com>
2016-04-10 02:11:59 +0200

    init

---
+++ app/home/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    templateUrl: 'app/home/welcome.component.html'
+})
+export /**
+ * WelcomeComponent
+ */
+class WelcomeComponent {
+    public pageTitle: string = "Welcome";
+}
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap } from 'angular2/platform/browser';
+
+// Main component
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
